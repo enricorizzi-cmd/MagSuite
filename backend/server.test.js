2c4089bc3e4afe7fc1f777f1c78c04347241eb00
enricorizzi-cmd <enrico.rizzi@dimensioneagricoltura.it>
2025-09-06 17:16:49 +0200

    eslint

---
+++ backend/server.test.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+const { start } = require('./server');
+
+let server;
+
+beforeAll(() => {
+  server = start(0);
+});
+
+afterAll((done) => {
+  server.close(done);
+});
+
+test('GET /health returns ok status', async () => {
+  const res = await request(server).get('/health');
+  expect(res.status).toBe(200);
+  expect(res.body).toEqual({ status: 'ok' });
+});

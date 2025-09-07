12f7f48b5508855139a267d25595537df670ea70
enricorizzi-cmd <enrico.rizzi@dimensioneagricoltura.it>
2025-09-07 18:02:39 +0200

    upd

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,14 +1,18 @@
+const fs = require('fs');
+const path = require('path');
 const { Pool } = require('pg');
 
+const ca = fs.readFileSync(path.join(__dirname, '..', '..', 'prod-ca-2021.crt'), 'utf8');
+
 const pool = process.env.DATABASE_URL
-  ? new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } })
+  ? new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: true, ca } })
   : new Pool({
       host: process.env.PGHOST,
       port: process.env.PGPORT,
       user: process.env.PGUSER,
       password: process.env.PGPASSWORD,
       database: process.env.PGDATABASE,
-      ssl: { rejectUnauthorized: false },
+      ssl: { rejectUnauthorized: true, ca },
     });
 
 module.exports = pool;
